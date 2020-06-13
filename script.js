a25f0252d00337046e9c68ffc1ed8aa85bcb2e25
Aaron Sims <aaron@adsimsllc.com>
2020-06-12 20:24:45 -0400

    documenting necessary variables and creating click event on start button to begin quiz

---
+++ script.js
@@ -0,0 +1,33 @@
+// necesary variables
+var body = document.body;
+var introEl = document.querySelector("#introduction");
+var timeEl = document.querySelector("#time");
+var mainEl = document.getElementById("#main");
+var beginEl = document.querySelector("#begin");
+var questionEl = document.querySelector("#question");
+var set1El = document.querySelector(".Set1");
+var ans1options = document.querySelector("#ans1options");
+var answer1El = document.querySelector("#answer1El");
+var userResults = document.querySelector("#user-results");
+var nameInput = document.querySelector("#name");
+var saveBtn = document.querySelector("#save");
+var msgDiv = document.querySelector("#msg");
+var userScoreEl = document.querySelector("#user-score");
+var userNameSpan = document.querySelector("#user-name");
+var registerEl = document.querySelector("#register");
+var resetBtn = document.querySelector("#reset-quiz");
+let score = 0;
+let incorrectEl = 0;
+var timerInterval;
+var secondsLeft = 60;
+
+// hides the results and register framework at the start of the quiz
+set1El.style.display = "none";
+registerEl.style.display = "none";
+
+// activating start button, starting the timer, and displaying first question
+beginEl.addEventListener("click", function () {
+  event.preventDefault;
+  setTime();
+  displayQuestion();
+});
